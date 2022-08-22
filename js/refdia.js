4156ec88b0c14d0ec62741fe156c6f4999627562
Luiz Carlos Santos <luizcd.santos58@gmail.com>
2022-08-22 09:03:02 -0300

    atualiza

--- js/refdia.js
+++ js/refdia.js
@@ -3,7 +3,7 @@ const greetingMessage = () => {
     let h = new Date().getHours();
     switch (true) {
       case h <= 5: return '&#xf123 Boa madrugada';
-      case h < 12: return '&&#xf118 Bom dia';
+      case h < 12: return '&#xf118 Bom dia';
       case h < 18: return '&#xf185 Boa tarde';
       default: return '&#xf186 Boa noite';
     }      
